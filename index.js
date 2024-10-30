f21719a4e5525fe17810436605f51ce679d3cf25
roblack66 <romeoasante66@gmail.com>
2024-10-30 14:24:45 +0000

    cors added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express"
 import mongoose from "mongoose"
 import userRouter from "./Routes/user.js"
+import cors from 'cors'
 
 await mongoose.connect(process.env.MONGO_URI)
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use(userRouter)
 
