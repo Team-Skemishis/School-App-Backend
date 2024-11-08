2289442845c462f13a8b1536de0218ecaa5e03f3
roblack66 <romeoasante66@gmail.com>
2024-11-08 18:45:49 +0000

    classes validator modified

--- Validators/class.js
+++ Validators/class.js
@@ -1,10 +1,15 @@
 import Joi from "joi";
+import joiObjectid from "joi-objectid";
+
+Joi.objectId = joiObjectid(Joi)
 
 export const addClassValidator = Joi.object({
     classNumber: Joi.string().required(),
-    classCategory: Joi.string().required()
+    classCategory: Joi.string().required(),
+    classTeacher:Joi.objectId().required()
 })
 export const updateClassValidator = Joi.object({
     classNumber: Joi.string(),
-    classCategory: Joi.string()
+    classCategory: Joi.string(),
+    classTeacher: Joi.objectId()
 })
